d352b850d017480c4b21bf3c7e8ee445f25b33ea
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2016-07-27 15:25:51 +0100

    added java cookbook attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,11 @@
+# QUAST attributes
 default['quast']['version'] = '4.2'
 default['quast']['install_path'] = '/usr/local'
 default['quast']['dir'] = default['quast']['install_path'] + '/' + 'quast-' + default['quast']['version']
 default['quast']['filename'] = "quast-#{node['quast']['version']}.tar.gz"
 default['quast']['url'] = "https://downloads.sourceforge.net/project/quast/#{node['quast']['filename']}"
 
-default['python']['version'] = '2.7.9'
+# attributes to override default behaviour of cookbook java ( needed to compile GAGE java classes)
+default['java']['install_flavor'] = 'oracle'
+default['java']['jdk_version'] = '8'
+default['java']['oracle']['accept_oracle_download_terms'] = true
