b4b9736f535f5cac242594ab478aa586f4898c4b
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2016-07-27 15:27:38 +0100

    Added tests

--- test/integration/default/inspec/default_spec.rb
+++ test/integration/default/inspec/default_spec.rb
@@ -1,28 +1,42 @@
-# Tests to check if the infrastructure we expect is available
 
 # Check that the installation directory was created successfully
-describe file(node['quast']['dir']) do
+describe file('/usr/local/quast-4.2') do
   it { should be_directory }
 end
 
 # QUAST automatically compiles all its sub-parts when needed (on the first use). Thus,
 # there is no special installation command for QUAST. However, we recommend you to run:
-describe command("python #{node['quast']['dir']}/quast.py --test") do # (if you plan to use quast.py)
+describe command('python /usr/local/quast-4.2/quast.py --test') do # (if you plan to use quast.py)
   its('exit_status') { should eq 0 }
-  its('stdout') { should match(//) }
+  its('stdout') { should match(/Thank you for using QUAST!/) }
 end
 
-describe command("python #{node['quast']['dir']}/quast.py --test-sv") do # (if you plan to use quast.py or metaquast.py with SV calling)
+describe command('python /usr/local/quast-4.2/quast.py --test-sv') do # (if you plan to use quast.py or metaquast.py with SV calling)
   its('exit_status') { should eq 0 }
-  its('stdout') { should match(//) }
+  its('stdout') { should match(/Thank you for using QUAST!/) }
 end
 
-describe command("python #{node['quast']['dir']}/metaquast.py --test") do # (if you plan to use quast.py or metaquast.py with reference genomes)
+describe command('python /usr/local/quast-4.2/metaquast.py --test') do # (if you plan to use quast.py or metaquast.py with reference genomes)
   its('exit_status') { should eq 0 }
-  its('stdout') { should match(//) }
+  its('stdout') { should match(/Thank you for using QUAST!/) }
 end
 
-describe command("python #{node['quast']['dir']}/metaquast.py --test-no-ref") do # (if you plan to use quast.py or metaquast.py without reference genomes)
+describe command('python /usr/local/quast-4.2/metaquast.py --test-no-ref') do # (if you plan to use quast.py or metaquast.py without reference genomes)
   its('exit_status') { should eq 0 }
-  its('stdout') { should match(//) }
+  its('stdout') { should match(/Thank you for using QUAST!/) }
+end
+
+# run over supplied test data
+describe command('/usr/local/quast-4.2/quast.py /usr/local/quast-4.2/test_data/contigs_1.fasta /usr/local/quast-4.2/test_data/contigs_2.fasta -R /usr/local/quast-4.2/test_data/reference.fasta.gz -G /usr/local/quast-4.2/test_data/genes.gff') do
+  its('exit_status') { should eq 0 }
+  its('stdout') { should match(/Thank you for using QUAST!/) }
+end
+
+describe file('/usr/local/quast-4.2/quast_results/latest/report.txt') do
+  it { should exist }
+  its('content') { should match 'All statistics are based on contigs of size >= 500 bp, unless otherwise noted' }
+  its('content') { should match 'Reference length' }
+  its('content') { should match '10000' }
+  its('content') { should match 'NGA50' }
+  its('content') { should match '1610' }
 end
