ff31e8545b95e043d1e072bbeade53178b3f3385
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2016-07-28 11:48:09 +0100

    First release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chef@eaglegenomics.com'
 license 'apache2'
 description 'Installs/Configures QUAST'
 long_description 'Installs/Configures QUAST'
-version '0.1.0'
+version '1.0.0'
 source_url 'https://github.com/EagleGenomics-cookbooks/QUAST'
 issues_url 'https://github.com/EagleGenomics-cookbooks/QUAST/issues'
 
