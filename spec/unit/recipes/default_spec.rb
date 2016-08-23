838e950ee9a94df4f8d1db531c615ce2d13898e7
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2016-08-23 16:28:22 +0100

    lowercase name

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -18,7 +18,7 @@
 
 require 'spec_helper'
 
-describe 'QUAST::default' do
+describe 'quast::default' do
   context 'When all attributes are default, on an unspecified platform' do
     let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
 
