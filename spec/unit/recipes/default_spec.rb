9ce597997464fd7bebf1f02b5328233970772b28
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2016-08-25 13:14:53 +0100

    See https://github.com/sethvargo/chefspec/issues/614

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -20,7 +20,8 @@ require 'spec_helper'
 
 describe 'quast::default' do
   context 'When all attributes are default, on an unspecified platform' do
-    let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
+    # let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
+    let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '14.04').converge(described_recipe) }
 
     # Make sure the converge works
     it 'converges successfully' do
